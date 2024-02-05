305e895d4fc19a6f72bfc4db3d2e1e330ea3c10f
Mayur <mah.mayur06@gmail.com>
2024-02-05 18:26:34 +0530

    added searching for posts

---
+++ src/NewPost.js
@@ -0,0 +1,30 @@
+
+const NewPost = ({ handleSubmit, postTitle, setPostTitle, postBody, setPostBody}) => {
+  return (
+        <main>
+            <h2>NewPost</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                    <input
+                        id = "postTitle"
+                        type="text"
+                        required
+                        value={postTitle}
+                        onChange={(e) => setPostTitle(e.target.value)}
+                />
+                
+                <label htmlFor="postBody">Post:</label>
+                <textarea
+                        id = "postBody"
+                        type="text"
+                        required
+                        value={postBody} 
+                        onChange={(e) => setPostBody(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </main>
+  )
+}
+
+export default NewPost
---
+++ src/PostPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const PostPage = ({ posts, handleDelete}) => {
+    const { id } = useParams();
+    const post = posts.find((post) => (post.id).toString() === id)
+
+    return (
+        <main className='PostPage'>
+            <article className='post'></article>
+            {post && 
+                <>
+                    <h2>{post.title}</h2>
+                    <p className="postDate">{post.datetime}</p>
+                    <p className="postBody">{post.body}</p>
+                    <button onClick={() => handleDelete(post.id)} >
+                        Delete Post
+                    </button>
+                </>
+            }
+            {!post &&
+                <>
+                    <h2>No post found</h2>
+                    <p>
+                        <Link to='/'>Link to Homepage</Link>
+                    </p>
+                </>
+            }
+        </main>
+    );
+}
+
+export default PostPage;
